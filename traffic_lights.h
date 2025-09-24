67cf43483ada4c3dc2068a7ff9097750afdbdad9
AndreaDonato <donato.1808606@studenti.uniroma1.it>
2025-09-24 14:22:24 +0200

    Automatic Commit

---
+++ traffic_lights.h
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+
+srand(time(NULL));
+
+// Funzione Aggiornamento (Azione tra stati CdM)
+void take_action()
+{
+
+}
+
+// Random Extractor
+int random(int max)
+{
+	return rand();
+}
+
+// Calcola Delta
+int delta(int *chain)
+{
+	return rand();	
+}
